deb4938000720f6368b5344145eb56e11c11bcb1
basurounaq11 <basurounaq11@gmail.com>
2018-02-16 11:19:26 -0500

    finished exercise

---
+++ basurounaq11/exercise.js
@@ -0,0 +1,15 @@
+var exercise = {};
+
+exercise.min = function(unordered) {
+    var min;
+
+    // Find the minimum value in an array
+    min = unordered.sort()[0];
+
+    return min;
+};
+
+var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
+exercise.min(unordered);
+
+module.exports = exercise;
